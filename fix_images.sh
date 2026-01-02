f6d3a94b65a1e18a130aaf21e1a9ec3a89a75132
Aron Girardelli <arongirardelli@gmail.com>
2026-01-02 15:56:11 -0300

    Initial commit: Curso de Releituras Gastronômicas site

---
+++ fix_images.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Starting copy process..."
+cp -v "imagens_receitas/Image_1.png" recipe_1.png
+cp -v "imagens_receitas/Image_2.png" recipe_2.png
+cp -v "imagens_receitas/Image_3.png" recipe_3.png
+cp -v "imagens_receitas/Image_4.png" recipe_4.png
+cp -v "imagens_receitas/Image_5.png" recipe_5.png
+cp -v "imagens_receitas/Image_6 .png" recipe_6.png
+cp -v "imagens_receitas/Image_7.png" recipe_7.png
+cp -v "imagens_receitas/Image_8.png" recipe_8.png
+cp -v "imagens_receitas/Image_9.png" recipe_9.png
+cp -v "imagens_receitas/Image_10.png" recipe_10.png
+cp -v "imagens_receitas/Image_11.png" recipe_11.png
+cp -v "imagens_receitas/Image_12.png" recipe_12.png
+cp -v "imagens_receitas/Image_13.png" recipe_13.png
+cp -v "imagens_receitas/Image_14.png" recipe_14.png
+cp -v "imagens_receitas/Image_15.png" recipe_15.png
+echo "Copy process finished."
+ls -la recipe_*.png
